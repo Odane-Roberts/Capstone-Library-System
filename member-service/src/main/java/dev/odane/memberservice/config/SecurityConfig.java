d68cbbd709152365f33551277d26dd74c9fba8b1
Odane Roberts <odaneroberts19@gmail.com>
2023-09-15 14:13:53 -0500

    Updated config files and services to facilitate communication between containers

--- member-service/src/main/java/dev/odane/memberservice/config/SecurityConfig.java
+++ member-service/src/main/java/dev/odane/memberservice/config/SecurityConfig.java
@@ -1,9 +1,9 @@
 package dev.odane.memberservice.config;
 
+import jakarta.servlet.annotation.WebFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@WebFilter
 public class SecurityConfig {
 
     private final JwtAuthConverter jwtAuthConverter;
